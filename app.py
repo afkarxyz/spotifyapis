2e35b811f543f4f1c7ba292df7da9f93d71f22d2
afkarxyz <mzamzamafkarhadiq@gmail.com>
2024-12-15 10:34:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, send_file
-from spotapi.public import Public
+from flask_cors import CORS
+from spotapi import Public
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
